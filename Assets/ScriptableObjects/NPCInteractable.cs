440a87088ce9f3291416a6bef14f26d9804069b5
rishavsarma5 <rishavsarma5@gmail.com>
2024-10-04 17:25:03 -0400

    added in polygon city pack

---
+++ Assets/ScriptableObjects/NPCInteractable.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NPC Interactable", menuName = "NPC Interactable")]
+public class NPCInteractable : ScriptableObject
+{
+   
+}
